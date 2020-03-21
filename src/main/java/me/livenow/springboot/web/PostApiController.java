7fdd61df9ee1e1f030a4fa382474fc02fe061ca9
Livenow14 <xowjd41@naver.com>
2020-03-21 20:35:52 +0900

    업데이트, 수정 기능 추가. 코드 상에 여러 체크사항 정리 완료

--- src/main/java/me/livenow/springboot/web/PostApiController.java
+++ src/main/java/me/livenow/springboot/web/PostApiController.java
@@ -2,19 +2,34 @@ package me.livenow.springboot.web;
 
 import lombok.RequiredArgsConstructor;
 import me.livenow.springboot.service.posts.PostsService;
+import me.livenow.springboot.web.dto.PostsResponseDto;
 import me.livenow.springboot.web.dto.PostsSaveRequestDto;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import me.livenow.springboot.web.dto.PostsUpdateRequestDto;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 public class PostApiController {
     private final PostsService postsService;
 
-    @PutMapping("/api/v1/posts")
+    @GetMapping("/")
+    public String hi(){
+        return "안녕?";
+    }
+
+    @PostMapping("/api/v1/posts")
     public long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto CheckId (@PathVariable Long id){
+        return postsService.findById(id);
     }
 
 }
