045c0457e8df7193174ec9e0994445021b174892
Livenow14 <xowjd41@naver.com>
2020-05-21 11:39:04 +0900

    RESTFUl API_ Ec2 서버 초기 DB설정

--- practice/src/main/java/me/livenow/springboot/initDB.java
+++ practice/src/main/java/me/livenow/springboot/initDB.java
@@ -28,8 +28,8 @@ public class initDB {
 
     @PostConstruct
     public void init(){
-        initService.dbInit1();
-      //  initService.dbInit2();
+       // initService.dbInit1();
+        initService.dbInitForEc2();
     }
 
     @Component
@@ -79,31 +79,33 @@ public class initDB {
             em.persist(alarm2);
 
         }
-        public void dbInit2() {
+        public void dbInitForEc2(){
             User user = new User();
-            user.setName("userB");
+            user.setName("userA");
             em.persist(user);
 
             Mlo mlo = new Mlo();
-            mlo.setMloName("mlo3");
+            mlo.setMloName("mlo1");
             mlo.setUser(user);
             em.persist(mlo);
 
             Mlo mlo2 = new Mlo();
-            mlo2.setMloName("mlo4");
+            mlo2.setMloName("mlo2");
             mlo2.setUser(user);
             em.persist(mlo);
 
-            Record record = new Record();
-            record.setFileName("record3");
-            record.setFileDownloadUrl("url1");
-            record.setFileType("image/jpeg");
+
+            Record record =new Record();
+            record.setFileName("file1");
+            record.setFileDownloadUrl("http://ec2-15-165-113-25.ap-northeast-2.compute.amazonaws.com:8080/downloadFile/file1.mp3");
+            record.setFileType("audio/mpeg");
+            record.setSize(5811859);
             record.setLocalDateTime(LocalDateTime.now());
             em.persist(record);
 
             Alarm alarm = new Alarm();
             alarm.setMlo(mlo);
-            alarm.setTumble("tumble3");
+            alarm.setTumble("9");
             alarm.setDate(LocalDateTime.now());
             alarm.setRecord(record);
             em.persist(alarm);
@@ -111,18 +113,12 @@ public class initDB {
 
             Alarm alarm2 = new Alarm();
             alarm2.setMlo(mlo);
-            alarm2.setTumble("tumble4");
+            alarm2.setDecibel("9");
+            alarm2.setTumble("2");
             alarm2.setDate(LocalDateTime.now());
             alarm2.setRecord(record);
             em.persist(alarm2);
 
-
-            Record record2 = new Record();
-            record2.setFileName("record4");
-            record.setFileDownloadUrl("url2");
-            record.setFileType("image/jpeg");
-            record2.setLocalDateTime(LocalDateTime.now());
-            em.persist(record2);
         }
     }
 }
